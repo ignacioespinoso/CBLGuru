7694f2819b9340ce0df2ca18c52493f70d2b42a8
Ignácio Espinoso Ribeiro <ignacioespinoso@gmail.com>
2019-04-18 13:23:02 -0300

    Initial Commit

---
+++ CBLplanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CBLplanner
+//
+//  Created by Ignácio Espinoso Ribeiro on 18/04/19.
+//  Copyright © 2019 Ignácio Espinoso Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

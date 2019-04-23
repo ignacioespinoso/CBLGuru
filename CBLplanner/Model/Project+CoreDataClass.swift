dce7bf50cc21a7a162d720df9fd5e44c4ceb8217
Ignácio Espinoso Ribeiro <ignacioespinoso@gmail.com>
2019-04-23 12:18:34 -0300

    Progress on creating entity-relationship model using Core Data.

---
+++ CBLplanner/Model/Project+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Project+CoreDataClass.swift
+//  CBLplanner
+//
+//  Created by Ignácio Espinoso Ribeiro on 23/04/19.
+//  Copyright © 2019 Ignácio Espinoso Ribeiro. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Project)
+public class Project: NSManagedObject {
+
+}
---
+++ Persistance/DAO/DAO.swift
@@ -0,0 +1,9 @@
+//
+//  DAO.swift
+//  CBLplanner
+//
+//  Created by Ignácio Espinoso Ribeiro on 23/04/19.
+//  Copyright © 2019 Ignácio Espinoso Ribeiro. All rights reserved.
+//
+
+import Foundation

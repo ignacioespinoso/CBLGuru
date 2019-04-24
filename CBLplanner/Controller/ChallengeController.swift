1ffe7150c42e8dfae918a4ceff8568872a19cbbe
Ignácio Espinoso Ribeiro <ignacioespinoso@gmail.com>
2019-04-24 10:32:46 -0300

    Finished Core Data integration. Already working with Engage cards.

--- CBLplanner/Controller/ChallengeController.swift
+++ CBLplanner/Controller/ChallengeController.swift
@@ -9,20 +9,36 @@
 import UIKit
 
 class ChallengeController: UIViewController {
-
+    /// Project to be stored in case of updating operation
+    @objc var project:ProjectData?
+    
     @IBOutlet weak var challenge: UITextField!
     
-    fileprivate let engageData = EngageData()
+    override func viewWillAppear(_ animated: Bool) {
+        // call super
+        super.viewWillAppear(animated)
+        
+        // get all seasons
+        ProjectServices.getFirstProject { (error, project) in
+            if (error == nil) {
+                // assign season list
+                self.project = project!
+                
+                OperationQueue.main.addOperation {
+                    // setup the ui with new data
+                    self.challenge.text = project?.challenge
+                }
+                
+            }
+            else {
+                print("Unable to find first project")
+            }
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func saveChallenge(_ sender: UIButton) {
-        if let value =  challenge.text {
-            self.engageData.setChallenge(newChallenge: value)
-        }
     }
     
     
@@ -30,5 +46,40 @@ class ChallengeController: UIViewController {
         let url = URL(string: "https://itunes.apple.com/us/app/cbl-nudge/id1403826548?mt=8")!
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
-
+    
+    /// Function to validate input data, creating and saving notifications, and dismissing.
+    @IBAction func saveProject(_ sender: UIButton) {
+        // in according to the operation, grab information from ui and persist it
+        
+        if ( self.project != nil ) {
+            
+            // get information from UI
+            self.project!.challenge = self.challenge.text!
+            
+            // update the information
+            ProjectServices.updateProject(project: self.project!) { error in
+                if (error != nil) {
+                    // treat error if necessary
+                }
+            }
+        }
+        else {
+            // initialize a new alarm get information from UI
+            self.project = ProjectData()
+            self.project!.challenge = self.challenge.text!
+            
+            //            DO I NEED TO INITIALIZE OTHER ELEMENTS AS WELL?
+            
+            // create new alarm
+            ProjectServices.createProject(project: self.project!) { error in
+                if (error != nil) {
+                    print("Error creating project")
+                    // treat error if necessary
+                }
+            }
+        }
+        
+        // go back to the Home
+        self.navigationController?.popViewController(animated: true)
+    }
 }

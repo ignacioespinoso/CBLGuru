1bc19d6e886ec3ab2a7fcfba8452da9dc4a281eb
Gabriel Ferreira <gabrielferreira@10-40-48-99.0200.ecps.br.iosda.org>
2019-05-10 12:49:12 -0300

    reminder funcionando, criando evento no calendario nativo e dando notificacao

--- CBLGuru/Controller/AddReminderViewController.swift
+++ CBLGuru/Controller/AddReminderViewController.swift
@@ -13,12 +13,15 @@ class AddReminderViewController: UIViewController {
     @objc var reminder : RemindersData?
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var date: UIDatePicker!
-    var tableView : UITableView? = nil
+    //var tableView : UITableView? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        if let thisReminder : RemindersData = self.reminder {
+            self.name.text = thisReminder.name
+            self.date.date = thisReminder.date!
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,12 +31,16 @@ class AddReminderViewController: UIViewController {
     @IBAction func saveReminder(_ sender: UIButton) {
         let eventStore = EKEventStore();
         
+        
+        let name = self.name.text
+        let date = self.date.date
+        
         eventStore.requestAccess(to: .event, completion: { (granted, error) in
             if (granted) && (error == nil){
                 let event = EKEvent(eventStore: eventStore)
-                event.title = self.name.text
-                event.startDate = self.date.date
-                event.endDate = self.date.date
+                event.title = name
+                event.startDate = date
+                event.endDate = date
                 event.addAlarm(EKAlarm(relativeOffset: 0))
                 event.calendar = eventStore.defaultCalendarForNewEvents
                 //event.addObserver(<#T##observer: NSObject##NSObject#>, forKeyPath: <#T##String#>, options: <#T##NSKeyValueObservingOptions#>, context: <#T##UnsafeMutableRawPointer?#>)
@@ -44,7 +51,6 @@ class AddReminderViewController: UIViewController {
                 } catch let error as NSError{
                     print("error \(error)")
                 }
-                print("Save event")
             } else {
                 print("error \(String(describing: error))")
             }
@@ -67,15 +73,16 @@ class AddReminderViewController: UIViewController {
         else {
             // initialize a new alarm get information from UI
             self.reminder = RemindersData()
-            self.reminder!.name = self.name.text!
+            self.reminder!.name = self.name.text
             self.reminder!.date = self.date.date
 
             // create new alarm
-            RemindersServices.updateProject(reminders: self.reminder!) { error in
+            RemindersServices.createProject(reminders: self.reminder!) { error in
                 if (error != nil) {
                     // treat error if necessary
                 }
             }
+            print(reminder!.date)
         }
 
         // go back to the alarm list
--- CBLGuru/Controller/ListRemindersViewController.swift
+++ CBLGuru/Controller/ListRemindersViewController.swift
@@ -8,116 +8,107 @@
 
 import UIKit
 
-class ListRemindersViewController: UIViewController {
-//    fileprivate var reminders: [RemindersData] = []
-//    @IBOutlet weak var table: UITableView!
-//    
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.hideKeyboardWhenTappedAround()
-//        // 1
-//        
-//        let nav = self.navigationController?.navigationBar
-//        
-//        // 2
-//        nav?.tintColor = UIColor.white
-//        nav?.prefersLargeTitles = true
-//        nav?.backgroundColor = UIColor(red:0.89, green:0.33, blue:0.15, alpha:1.0)
-//        nav?.barTintColor = UIColor(red:0.67, green:0.81, blue:0.11, alpha:1.0)
-//        //        nav?.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-//        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
-//        nav?.largeTitleTextAttributes = textAttributes
-//        nav?.titleTextAttributes = textAttributes
-//    }
-//    
-//    override func viewWillAppear(_ animated: Bool) {
-//        // call super
-//        super.viewWillAppear(animated)
-//        
-//        // get all seasons
-//        RemindersServices.getAllProjects { (error, reminders) in
-//            if (error == nil) {
-//                // assign season list
-//                self.reminders = reminders!
-//                
-//                // reload table view with season information
-//                DispatchQueue.main.async {
-//                    self.table.reloadData()
-//                }
-//            }
-//            else {
-//                // display error here because it was not possible to load season list
-//                print("Error retrieving content")
-//            }
-//        }
-//    }
-//    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // check for the correct transition
-//        if (segue.identifier == "addOrEditReminder") {
-//            // cast destination controller and setup initial data
-//            let destinationViewController:AddReminderViewController = segue.destination as! AddReminderViewController
-//            destinationViewController.navigationItem.title = (sender is RemindersData) ? "Edit Reminder" : "Add Reminder"
-//            destinationViewController.reminder = sender as? RemindersData
-//        }
-//    }
-//}
-//
-//extension ListQuestionsViewController: UITableViewDataSource, UITableViewDelegate {
-//    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return self.reminders.count
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        // get a new cell
-//        let cell:ReminderTableViewCell = table.dequeueReusableCell(withIdentifier: "ReminderCells", for: indexPath) as! ReminderTableViewCell
-//        
-//        // get the season data to be displayed
-//        if let reminder:RemindersData = self.reminders[indexPath.row] {
-//            // fill cell with extracted information
-//            cell.questionLabel.text = reminder.question
-//            cell.resourceLabel.text = reminder.resource
-//            cell.activityLabel.text = reminder.activity
-//        }
-//        
-//        
-//        
-//        return cell
-//    }
-//    
-//    func tableView(_ tableView: UITableView, titleForHeaderInSection section:Int) -> String? {
-//        return "Reminder questions already created:"
-//    }
-//    
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        performSegue(withIdentifier: "addOrEditReminder", sender: self.reminders[indexPath.row])
-//    }
-//    
-//    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//        return true
-//    }
-//    
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if (editingStyle == .delete) {
-//            // get season that will be deleted
-//            let reminder:ReminderData = self.reminders[indexPath.row]
-//            
-//            // remove season from database
-//            ReminderServices.deleteProject(reminder:reminder) { (error) in
-//                if ( error == nil ) {
-//                    // remove element from array
-//                    self.reminders.remove(at: indexPath.row)
-//                    
-//                    // remove element from table view
-//                    self.table.deleteRows(at: [indexPath], with: .fade)
-//                }
-//                else {
-//                    // manage a possible problem to remove the item from database
-//                }
-//            }
-//        }
-//    }
+class ListRemindersViewController: UITableViewController {
+    fileprivate var reminders: [RemindersData] = []
+    @IBOutlet weak var table: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hideKeyboardWhenTappedAround()    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // call super
+        super.viewWillAppear(animated)
+        
+        print(self.reminders)
+
+        // get all seasons
+        RemindersServices.getAllProjects { (error, reminders) in
+            if (error == nil) {
+                // assign season list
+                self.reminders = reminders!
+                
+                // reload table view with season information
+                DispatchQueue.main.async {
+                    self.table.reloadData()
+                }
+            }
+            else {
+                // display error here because it was not possible to load season list
+                print("Error retrieving content")
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // check for the correct transition
+        if (segue.identifier == "addOrEditReminder") {
+            // cast destination controller and setup initial data
+            let destinationViewController:AddReminderViewController = segue.destination as! AddReminderViewController
+            destinationViewController.navigationItem.title = (sender is RemindersData) ? "Edit Reminder" : "Add Reminder"
+            destinationViewController.reminder = sender as? RemindersData
+        }
+    }
+}
+
+extension ListRemindersViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.reminders.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // get a new cell
+        let cell:RemindersTableViewCell = table.dequeueReusableCell(withIdentifier: "ReminderCell", for: indexPath) as! RemindersTableViewCell
+        
+        // fill cell with extracted information
+        cell.reminderName.text = self.reminders[indexPath.row].name
+        var date = self.reminders[indexPath.row].date
+        date?.addTimeInterval(Double(TimeZone.current.secondsFromGMT()))
+        cell.reminderDate.text = date?.description
+        
+        
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section:Int) -> String? {
+        return "Reminders already created:"
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "addOrEditReminder", sender: self.reminders[indexPath.row])
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            // get season that will be deleted
+            let reminder : RemindersData = self.reminders[indexPath.row]
+            
+            // remove season from database
+            RemindersServices.deleteProject(reminders: reminder) { (error) in
+                if ( error == nil ) {
+                    // remove element from array
+                    self.reminders.remove(at: indexPath.row)
+                    
+                    // remove element from table view
+                    self.table.deleteRows(at: [indexPath], with: .fade)
+                }
+                else {
+                    // manage a possible problem to remove the item from database
+                }
+            }
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(self.reminders)
+    }
 
 }

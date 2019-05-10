e448e31fa041a5d8da3a9d9dfce09c84094a9c82
Gabriel Ferreira <gabrielferreira@Gabriels-MacBook-Pro.local>
2019-05-10 04:32:19 -0300

    reminder salvo no calendario com alerta, falta aparecer na lista e deletar

--- CBLGuru/Controller/AddReminderViewController.swift
+++ CBLGuru/Controller/AddReminderViewController.swift
@@ -7,40 +7,57 @@
 //
 
 import UIKit
+import EventKit
 
 class AddReminderViewController: UIViewController {
     @objc var reminder : RemindersData?
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var date: UIDatePicker!
+    var tableView : UITableView? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
+    
     @IBAction func saveReminder(_ sender: UIButton) {
-        // TODO: check if the form was correctly fullfilled
-        // in according to the operation, grab information from ui and persist it
+        let eventStore = EKEventStore();
+        
+        eventStore.requestAccess(to: .event, completion: { (granted, error) in
+            if (granted) && (error == nil){
+                let event = EKEvent(eventStore: eventStore)
+                event.title = self.name.text
+                event.startDate = self.date.date
+                event.endDate = self.date.date
+                event.addAlarm(EKAlarm(relativeOffset: 0))
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                //event.addObserver(<#T##observer: NSObject##NSObject#>, forKeyPath: <#T##String#>, options: <#T##NSKeyValueObservingOptions#>, context: <#T##UnsafeMutableRawPointer?#>)
+                
+                do {
+                    try eventStore.save(event, span: .thisEvent)
+                    
+                } catch let error as NSError{
+                    print("error \(error)")
+                }
+                print("Save event")
+            } else {
+                print("error \(String(describing: error))")
+            }
+        })
+        
         if ( self.reminder != nil ) {
-            print("nem fodendo")
             // get information from UI
             self.reminder!.name = self.name.text!
             self.reminder!.date = self.date.date
-            
-            
+
+
             // update the information
-            
+
             RemindersServices.updateProject(reminders: self.reminder!) { error in
                 if (error != nil) {
                     // treat error if necessary
@@ -52,7 +69,7 @@ class AddReminderViewController: UIViewController {
             self.reminder = RemindersData()
             self.reminder!.name = self.name.text!
             self.reminder!.date = self.date.date
-            
+
             // create new alarm
             RemindersServices.updateProject(reminders: self.reminder!) { error in
                 if (error != nil) {
@@ -60,7 +77,7 @@ class AddReminderViewController: UIViewController {
                 }
             }
         }
-        
+
         // go back to the alarm list
         self.navigationController?.popViewController(animated: true)
     }
